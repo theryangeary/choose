1d4fd6f48ee7ea789910f73b52f87b8243c1c6e6
Ryan Geary <rtgnj42@gmail.com>
2019-09-10 23:51:21 -0400

    Ignore all whitespace in testing

--- test/test.sh
+++ test/test.sh
@@ -6,8 +6,11 @@ orig_dir="$(pwd)"
 cd "$(git rev-parse --show-toplevel)"
 cargo build
 
-diff -Z <(cargo run -- 0:2 -i ${test_dir}/lorem.txt) <(cat "${test_dir}/choose_0:2.txt")
-diff -Z <(cargo run -- 0 3 -i ${test_dir}/lorem.txt) <(cat "${test_dir}/choose_0_3.txt")
-diff -Z <(cargo run -- :2 -i ${test_dir}/lorem.txt) <(cat "${test_dir}/choose_:2.txt")
-diff -Z <(cargo run -- 9 3 -i ${test_dir}/lorem.txt) <(cat "${test_dir}/choose_9_3.txt")
-diff -Z <(cargo run -- 9 -i ${test_dir}/lorem.txt) <(cat "${test_dir}/choose_9.txt")
+diff -w <(cargo run -- 0:2 -i ${test_dir}/lorem.txt) <(cat "${test_dir}/choose_0:2.txt")
+diff -w <(cargo run -- 0 3 -i ${test_dir}/lorem.txt) <(cat "${test_dir}/choose_0_3.txt")
+diff -w <(cargo run -- :2 -i ${test_dir}/lorem.txt) <(cat "${test_dir}/choose_:2.txt")
+diff -w <(cargo run -- 9 3 -i ${test_dir}/lorem.txt) <(cat "${test_dir}/choose_9_3.txt")
+diff -w <(cargo run -- 9 -i ${test_dir}/lorem.txt) <(cat "${test_dir}/choose_9.txt")
+# add test for reverse range
+# add tests for different delimiters
+# add tests using piping
