a802ef41970500ec42e27f805d5402be48793443
Ryan Geary <rtgnj42@gmail.com>
2020-04-05 17:28:10 -0400

    Update OFS test to reflect single character requirement

--- src/choice.rs
+++ src/choice.rs
@@ -575,11 +575,11 @@ mod tests {
 
         #[test]
         fn print_1_to_3_with_output_field_separator() {
-            let config = Config::from_iter(vec!["choose", "1:3", "-o", "#$%"]);
+            let config = Config::from_iter(vec!["choose", "1:3", "-o", "#"]);
             let mut handle = BufWriter::new(MockStdout::new());
             config.opt.choice[0].print_choice(&String::from("a b c d"), &config, &mut handle);
             assert_eq!(
-                String::from("b#$%c#$%d"),
+                String::from("b#c#d"),
                 MockStdout::str_from_buf_writer(handle)
             );
         }
