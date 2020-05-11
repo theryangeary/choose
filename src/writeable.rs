a66621380b370c011de01a6d22dc2ebc638d0019
Ryan Geary <rtgnj42@gmail.com>
2020-05-11 16:41:23 -0400

    Add character-wise `choose`ing
    Alphabetize structopt options
    
    Add character-wise tests
    
    Add character-wise switch
    
    Add print-after-end test
    
    Add empty default separator for char-wise mode
    
    Add char-wise forward and negative printing
    
    Add pure reverse printing
    
    Change to char_wise to user `char` instead of `u8`
    
    Adds support for unicode (read: emojis)
    
    Adds a newline char to end of each char-wise test because that's how it
    is
    
    Add writing traits for better code structure
    
    Merge repetitive codepaths with generics
    
    Unify print_choice_* funtion names
    
    Reorder functions in choice module
    
    Rename variable to avoid name confusion
    
    Make default case for loop more readable
    
    Abstract default case print loop
    
    Add e2e test
    
    Move vec create to print_choice_negative

---
+++ src/writeable.rs
@@ -0,0 +1,20 @@
+pub trait Writeable {
+    fn to_byte_buf(&self) -> Box<[u8]>;
+}
+
+impl Writeable for &str {
+    fn to_byte_buf(&self) -> Box<[u8]> {
+        return Box::from(self.as_bytes());
+    }
+}
+
+impl Writeable for char {
+    fn to_byte_buf(&self) -> Box<[u8]> {
+        let mut buf = [0; 4];
+        return self
+            .encode_utf8(&mut buf)
+            .to_owned()
+            .into_boxed_str()
+            .into_boxed_bytes();
+    }
+}
