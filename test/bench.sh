d6e3a50ba1c2e6bc47d8843e3146550bc1551d04
Ryan Geary <rtgnj42@gmail.com>
2025-08-27 00:00:00 +0000

    update bench script

--- test/bench.sh
+++ test/bench.sh
@@ -2,9 +2,12 @@
 cargo build --release # always be up to date
 output="bench_output"
 mkdir -p $output
-inputs=($(find test -name "long*txt" | sort -r))
-for i in {0..4}
-do
-  echo ${inputs[$i]}
-  bench "target/release/choose 3:5 -i ${inputs[$i]}"  > $output/$1$i.bench
+inputs="$(find test -name "long*txt")"
+for i in $inputs; do
+  echo ${i}
+  bench "target/release/choose 3:5 -i ${i}" > $output/choose_$(basename $i .txt).bench
+  bench "cut -f 4-6 -d ' ' ${i}" > $output/cut_$(basename $i .txt).bench
+  bench "awk '{print $4 $5 $6}' ${i}" > $output/awk_$(basename $i .txt).bench
 done
+
+grep time $output/* | sort -r
