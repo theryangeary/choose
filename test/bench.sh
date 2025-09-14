1a3b2d7ff51fb528821255cb31976d3a376e15cf
Ryan Geary <rtgnj42@gmail.com>
2025-09-14 00:00:00 +0000

    improve benchmark test output

--- test/bench.sh
+++ test/bench.sh
@@ -5,11 +5,14 @@ mkdir -p $output
 inputs="$(find test -name "long*txt")"
 for i in $inputs; do
   echo ${i}
-  bench "target/release/choose 3:5 -i ${i}" > $output/choose_$(basename $i .txt).bench
+  bench "target/release/choose 3:5 -i ${i}" > $output/choose$(basename $i .txt).bench
   bench "cut -f 4-6 -d ' ' ${i}" > $output/cut_$(basename $i .txt).bench
   bench "gcut -f 4-6 -d ' ' ${i}" > $output/gcut_$(basename $i .txt).bench
   bench "awk '{print $4 $5 $6}' ${i}" > $output/awk_$(basename $i .txt).bench
   bench "gawk '{print $4 $5 $6}' ${i}" > $output/gawk_$(basename $i .txt).bench
 done
 
-grep time $output/* | sort -r | column -t
+grep time $output/* | awk '{ value=$2; unitvalue=$3; };
+    / s/ { m=1000 }; / ms/ { m=1 };
+    { sub(" s","",unitvalue); value=value*m;
+    print value " " $0; }' | choose -f '(long| )' 2 0 1 3: | sort -V | choose -f '(\.b|/)' 0 2: | column -t
