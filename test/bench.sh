937b6544879f7c978dc2aa05fb71619f51c12c14
Ryan Geary <rtgnj42@gmail.com>
2025-09-15 00:00:00 +0000

    rename bench outputs for cleaner formatting

--- test/bench.sh
+++ test/bench.sh
@@ -6,10 +6,10 @@ inputs="$(find test -name "long*txt")"
 for i in $inputs; do
   echo ${i}
   bench "target/release/choose 3:5 -i ${i}" > $output/choose$(basename $i .txt).bench
-  bench "cut -f 4-6 -d ' ' ${i}" > $output/cut_$(basename $i .txt).bench
-  bench "gcut -f 4-6 -d ' ' ${i}" > $output/gcut_$(basename $i .txt).bench
-  bench "awk '{print $4 $5 $6}' ${i}" > $output/awk_$(basename $i .txt).bench
-  bench "gawk '{print $4 $5 $6}' ${i}" > $output/gawk_$(basename $i .txt).bench
+  bench "cut -f 4-6 -d ' ' ${i}" > $output/cut$(basename $i .txt).bench
+  bench "gcut -f 4-6 -d ' ' ${i}" > $output/gcut$(basename $i .txt).bench
+  bench "awk '{print $4 $5 $6}' ${i}" > $output/awk$(basename $i .txt).bench
+  bench "gawk '{print $4 $5 $6}' ${i}" > $output/gawk$(basename $i .txt).bench
 done
 
 grep time $output/* | awk '{ value=$2; unitvalue=$3; };
